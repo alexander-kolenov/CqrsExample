777713cdd3da38985e46b0c825d2349284a4940e
kolenov.ab <kolenov.ab@arkan.ru>
2024-10-11 15:04:46 +0300

    Add TransactionScope example

--- Application/RequestToSetWeatherByRegionNameHandler.cs
+++ Application/RequestToSetWeatherByRegionNameHandler.cs
@@ -3,6 +3,7 @@ using Domain.Requests.ReadonlyWeatherRepository;
 using Domain.Requests.WeatherRepository;
 using MediatR;
 using Microsoft.Extensions.Logging;
+using System.Transactions;
 
 namespace Application;
 
@@ -22,8 +23,16 @@ public class RequestToSetWeatherByRegionNameHandler :
 
    public async Task<Unit> Handle(RequestToSetWeatherByRegionName request, CancellationToken cancellationToken)
    {
+      using var scope  = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
+
       var region = await _mediator.Send(new RequestToGetRegionByName(request.RegionName), cancellationToken);
       var weather = await _mediator.Send(new RequestToSetWeatherByRegionId(region.Id, request.Weather), cancellationToken);
+
+      if (request.Weather.Temperature > 50)
+         throw new ArgumentException(nameof(request.Weather.Temperature), "Temperature exceed limit 50 Celsius degrees");
+
+      scope.Complete();
+
       return Unit.Value;
    }
 }
